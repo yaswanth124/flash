6115fce4c4b97995d4e010899a73aa0d1db45809
kotha <kotha@LAPTOP-UD9ELHT7>
2020-08-15 19:39:58 +0530

    committ to master branch

--- DemoGit/src/com/app/Demo1.java
+++ DemoGit/src/com/app/Demo1.java
@@ -2,6 +2,7 @@ package com.app;
 
 public class Demo1 {
 	public static void main(String[] args) {
+		System.out.println(" welcome to git class");
 		System.out.println("cheek1");
 		System.out.println(" cheek 2 ");
 		System.out.println("cheek 3");
