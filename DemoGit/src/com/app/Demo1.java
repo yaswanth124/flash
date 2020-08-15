29b745614d30ad1a9cfb517e1eb8436e4e06e1b2
kotha <kotha@LAPTOP-UD9ELHT7>
2020-08-15 19:34:29 +0530

    cheek msg has added

--- DemoGit/src/com/app/Demo1.java
+++ DemoGit/src/com/app/Demo1.java
@@ -2,7 +2,9 @@ package com.app;
 
 public class Demo1 {
 	public static void main(String[] args) {
-		System.out.println(" class of git ");
+		System.out.println("cheek1");
+		System.out.println(" cheek 2 ");
+		System.out.println("cheek 3");
 	}
 
 }
